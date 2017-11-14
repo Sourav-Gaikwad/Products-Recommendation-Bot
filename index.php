d514455558f0cb611cf239bef8ef10f63054f66c
Sourav-Gaikwad <32496679+Sourav-Gaikwad@users.noreply.github.com>
2017-11-15 01:52:10 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,10 @@
 <?php
 function operate($param1, $param2) 
 {
+    if ((($param1 >= 1) && ($param1 <= 9)) && ($param2 == null)) 
+    {
+        $param1 = $param1 * 1000;
+    }
     if ((($param1 >= 4900) && ($param1 <= 7500)) || (($param1 == 5000) && ($param2 == 10000))) 
     {
         $ret = "Hey, Redmi 4A can be a good choice for you. The most attractive feature of this phone is- 2GB RAM (For 16-GB variant) \n & 3GB Ram (for 32-GB variant) \n So, this is the best suitable choice for you in this range..!! \n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n
